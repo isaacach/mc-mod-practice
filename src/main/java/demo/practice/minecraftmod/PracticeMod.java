20f33cbc465c9fcaef2f8f51210dd50a715f6088
isaacach <124728947+isaacach@users.noreply.github.com>
2023-09-07 16:45:13 -0500

    added example item

---
+++ src/main/java/demo/practice/minecraftmod/PracticeMod.java
@@ -0,0 +1,18 @@
+package demo.practice.minecraftmod;
+
+import demo.practice.minecraftmod.init.ItemInit;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(PracticeMod.MODID)
+public class PracticeMod {
+    public static final String MODID = "practicemod";
+
+    public PracticeMod() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ItemInit.ITEMS.register(bus);
+    }
+
+}
